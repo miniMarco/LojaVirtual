575d1fbb11ff38ab41e043fa5098be25c972eb09
Marco Pires <atpires34@yahoo.com.br>
2021-06-21 13:47:58 -0300

    commit - entityframework, listview de produtos

---
+++ LojaVirtual.Dominio/Repositorio/Context.cs
@@ -0,0 +1,17 @@
+﻿using LojaVirtual.Dominio.Entidade;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace LojaVirtual.Dominio.Repositorio
+{
+    public class Context : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
---
+++ LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly Context _context = new Context();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+    }
+}
